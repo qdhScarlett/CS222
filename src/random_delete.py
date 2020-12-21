d801fbbe1ce8b30e962e903ff1bb8cc39e9e40e0
wenyixue <64853119+wenyixue@users.noreply.github.com>
2020-12-21 23:00:48 +0800

    Update random_delete.py

--- src/random_delete.py
+++ src/random_delete.py
@@ -18,7 +18,7 @@ np.random.seed(seed)
 tf.set_random_seed(seed)
 parser = argparse.ArgumentParser()
 
-
+'''
 # cora
 parser.add_argument('--dataset', type=str, default='cora', help='which dataset to use')
 parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
@@ -30,6 +30,8 @@ parser.add_argument('--lpa_weight', type=float, default=10, help='weight of LP r
 parser.add_argument('--dropout', type=float, default=0.2, help='dropout rate')
 parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
 '''
+
+'''
 # citeseer
 parser.add_argument('--dataset', type=str, default='citeseer', help='which dataset to use')
 parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
@@ -40,17 +42,19 @@ parser.add_argument('--l2_weight', type=float, default=5e-4, help='weight of l2 
 parser.add_argument('--lpa_weight', type=float, default=1, help='weight of LP regularization')
 parser.add_argument('--dropout', type=float, default=0, help='dropout rate')
 parser.add_argument('--lr', type=float, default=0.2, help='learning rate')
+'''
+
 # pubmed
 parser.add_argument('--dataset', type=str, default='pubmed', help='which dataset to use')
 parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
 parser.add_argument('--dim', type=int, default=32, help='dimension of hidden layers')
 parser.add_argument('--gcn_layer', type=int, default=2, help='number of GCN layers')
-parser.add_argument('--lpa_iter', type=int, default=1, help='number of LPA iterations')
+parser.add_argument('--lpa_iter', type=int, default=20, help='number of LPA iterations')
 parser.add_argument('--l2_weight', type=float, default=2e-4, help='weight of l2 regularization')
 parser.add_argument('--lpa_weight', type=float, default=1, help='weight of LP regularization')
 parser.add_argument('--dropout', type=float, default=0, help='dropout rate')
 parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
-'''
+
 
 '''
 # coauthor-cs
@@ -114,12 +118,12 @@ normalized_weight =np.array(LPA_adj[1])
 normalized_weight=normalized_weight-np.min(normalized_weight)
 normalized_weight=normalized_weight/np.max(normalized_weight)
 
-for i in range(len(adj[0])//7):
-    a=random.randint(0,2707)
+for i in range(3000):
+    a=random.randint(0,labels.shape[1]-1)
     np.delete(edge_pair,a,0)
     np.delete(normalized_weight,a,None) 
 print(edge_pair,normalized_weight)
-SP_adj = tf.SparseTensor(indices = edge_pair.astype(np.float64),values = normalized_weight.astype(np.float64),dense_shape=[[features[2][0], features[2][0]]])
+SP_adj = tf.SparseTensor(indices = edge_pair.astype(np.float64),values = normalized_weight.astype(np.float64),dense_shape=[features[2][0], features[2][0]])
 
 
 train_GCN(args, features, labels, SP_adj, train_mask, val_mask, test_mask)
